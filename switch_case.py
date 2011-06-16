956069f13a1ba52314143819ab06adb56ddec1ae
David J Felix <felix.davidj@gmail.com>
2011-06-16 16:46:07 -0400

    Added actual changes. Declared my copyright (as substaintal changes have been made) Changing Readme to ReStructuredText format

--- switch_case.py
+++ switch_case.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-
-# Copyright (c) 2011, Jerry Felix
+#
+# Copyright (c) 2011, David J Felix
 # All rights reserved.
-
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #    * Redistributions of source code must retain the above copyright
@@ -11,64 +11,66 @@
 #    * Redistributions in binary form must reproduce the above copyright
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the <organization> nor the
-#      names of its contributors may be used to endorse or promote products
-#      derived from this software without specific prior written permission.
+#    * Neither the name of the <organization> nor the names of its contributors
+#      may be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-class Switch():
+class Switch(object):
     """
     """
+    def __call__(self, value):
+        if verbose:
+            if self.switched_value == value:
+                if self.fall_through:
+                    self.cont = True
+                    self.default = True
 
-    fall_through_flag = False
-    test_set = set()
-    
-    def __init__(self, switch_data, fall_through = True):
-        """ 
-        """
-        self.switch_data = switch_data
-        self.fall_through = fall_through
-        
-    def __call__(self, *case_args):
-        """       
-        """
-        if not self.fall_through:
-            if not self.fall_through_flag:
-                self.test_set = set()
+                else:
+                    self.default = False
 
-            else:
-                self.fall_through_flag = False
-
-        self.test_set = self.test_set.union(set(case_args))
-        for any in self.test_set:
-            if any and isinstance(any, bool):
+                self.valid_case = True
                 return True
 
-            try:
-                if any == self.switch_data:
-                    return True
+            elif self.cont:
+                return True
 
-            except(TypeError):
-                pass
+            else:
+                return False
 
-        return False
+        else:
+            return bool(value)
 
-    def break(self):
-        """
-        """
-        self.test_set = set()
+    def __init__(self, value, verbose = False, fall_through = True):
+        self.switched_value = value
+        self.cont = False
+        self.default = True
+        self.valid_case = False
 
-    def continue(self):
-        """
-        """
-        self.fall_through_flag = True
+        if verbose:
+            self.__call__ = self._verbose_call
+
+        self.fall_through = fall_through
+
+    def fall(self):
+        self.cont = True
+
+    def stop(self):
+        self.cont = False
+        self.default = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
